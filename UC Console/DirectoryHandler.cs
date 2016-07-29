b41803799c6bdd1e2ce379a58ec18593a4d5cb88
Alexsofar <alesofar@yandex.ru>
2016-07-30 00:14:45 +0800

    First commit this project

---
+++ UC Console/DirectoryHandler.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace UC_Console
+{
+    class DirectoryHandler
+    {
+        private string DirPath;
+
+        public int CountItem;
+
+        private List<string> DirsList;
+
+        public DirectoryHandler()
+        {
+            this.DirPath = @"c:\\";
+            this.DirsList = new List<string>(Directory.EnumerateDirectories(DirPath));
+            this.CountItem = DirsList.Count;
+        }
+
+        public void OpenDirectory()
+        {
+            this.DirsList = new List<string>(Directory.EnumerateDirectories(DirPath));
+
+            Int32 listNum = 0;
+
+            foreach (var dir in DirsList)
+            {
+                DirectoryInfo dirAttrib = new DirectoryInfo(dir);
+
+                // Doesn't shows hidden system directories
+                if ((dirAttrib.Attributes & FileAttributes.Hidden) != FileAttributes.Hidden)
+                {
+                    Console.WriteLine("[{0}] {1}", listNum++, dir.Substring(dir.LastIndexOf("\\") + 1));
+                }
+            }
+
+            Console.WriteLine("\nЧтобы выбрать введите номер каталог [n]");
+            Console.WriteLine("\nЧтобы выйти из текущего каталога введите [.]");
+        }
+
+        public void OpenDirectory(int cmd)
+        {
+            Console.Clear();
+            this.DirPath = DirsList[cmd];
+            this.OpenDirectory();
+        }
+
+        public void PrevDirectory()
+        {
+            if (DirPath.Length != 4)
+            {
+                Console.Clear();
+                this.DirPath = DirPath.Remove(DirPath.LastIndexOf("\\"));
+                this.OpenDirectory();
+            }
+        }
+    }
+}
--- UC Console/Program.cs
+++ UC Console/Program.cs
@@ -8,8 +8,40 @@ namespace UC_Console
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            DirectoryHandler dir = new DirectoryHandler();
+
+            dir.OpenDirectory();
+
+            string cmd = "";
+
+            for (int i = 0; cmd != "x"; i++)
+            {
+                cmd = Console.ReadLine();
+
+                try
+                {
+                    if (cmd == ".")
+                        dir.PrevDirectory();
+                    else if (Int32.Parse(cmd) < dir.CountItem)
+                    {
+                        dir.OpenDirectory(Int32.Parse(cmd));
+                    }
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Команда не опознана :(");
+                }
+                catch (ArgumentOutOfRangeException)
+                {
+                    Console.WriteLine("Папка с индексом {0} не существует", cmd);
+                }
+                catch (UnauthorizedAccessException)
+                {
+                    Console.WriteLine("Доступ запрещен!");
+                }
+            }
         }
     }
 }
